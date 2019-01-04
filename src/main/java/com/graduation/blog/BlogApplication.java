ed19968cfcbcccc060fc408e7fddb80859064c00
成都 夏川 <2116364562@qq.com>
2019-01-04 16:30:27 +0800

    sss

--- src/main/java/com/graduation/blog/BlogApplication.java
+++ src/main/java/com/graduation/blog/BlogApplication.java
@@ -2,10 +2,12 @@ package com.graduation.blog;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.graduation.blog.dao")
+@EnableAutoConfiguration
 public class BlogApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/graduation/blog/controller/WpUsersController.java
+++ src/main/java/com/graduation/blog/controller/WpUsersController.java
@@ -2,9 +2,12 @@ package com.graduation.blog.controller;
 
 import com.graduation.blog.domain.WpUsers;
 import com.graduation.blog.service.WpUsersService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,12 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  **/
 @RestController
+@RequestMapping("/wpUsers")
+@Api(value = "用户查询", tags = "用户查询")
 public class WpUsersController {
 
   @Autowired
   private WpUsersService wpUsersService;
 
   @GetMapping("/select/{id}")
+  @ApiOperation(value = "查询用户", notes = "查询用户")
   public WpUsers selectByPrimKey(@PathVariable("id") Long id) {
     WpUsers wpUsers = wpUsersService.selectByPrimaryKey(id);
     return wpUsers;
