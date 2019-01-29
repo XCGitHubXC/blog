0a49c4325c2d7e42dce7ec4d06a147f6eb2963d1
成都 夏川 <2116364562@qq.com>
2019-01-29 15:50:36 +0800

    用户积分

--- src/main/java/com/graduation/blog/domain/User.java
+++ src/main/java/com/graduation/blog/domain/User.java
@@ -89,6 +89,15 @@ public class User extends BaseObject {
   private Date birthday;
 
 
+  /**
+   * 积分
+   */
+  @ApiModelProperty(value = "积分")
+  private Integer score;
+
+
+
+
   @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
   public Date getBirthday() {
