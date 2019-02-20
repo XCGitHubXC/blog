d323e53a286828cbe9ffa61ffa0fffb71b631e8e
成都 夏川 <2116364562@qq.com>
2019-02-20 09:49:34 +0800

    跨域修改

--- src/main/java/com/graduation/blog/BlogApplication.java
+++ src/main/java/com/graduation/blog/BlogApplication.java
@@ -1,11 +1,15 @@
 package com.graduation.blog;
 
-import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.graduation.blog.dao")
@@ -13,6 +17,31 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAutoConfiguration
 @EnableTransactionManagement
 public class BlogApplication {
+
+
+  private CorsConfiguration buildConfig() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.addAllowedOrigin("*");
+    corsConfiguration.addAllowedHeader("*");
+    corsConfiguration.addAllowedMethod("*");
+    // 这两句不加不能跨域上传文件
+    corsConfiguration.setAllowCredentials(true);
+    // 加上去就可以了
+    corsConfiguration.setMaxAge(3600L);
+    return corsConfiguration;
+  }
+
+  /**
+   * 跨域过滤器
+   * @return
+   */
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", buildConfig());
+    return new CorsFilter(source);
+  }
+
 
   public static void main(String[] args) {
     SpringApplication.run(BlogApplication.class, args);
--- src/main/java/com/graduation/blog/config/CorsConfigure.java
+++ src/main/java/com/graduation/blog/config/CorsConfigure.java
@@ -1,22 +1,22 @@
-package com.graduation.blog.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-/**
- * @Author: xiachuan
- * @Date: 2019/2/18
- * @Description:
- */
-@Configuration
-public class CorsConfigure extends WebMvcConfigurerAdapter {
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
-        .allowCredentials(true).maxAge(3600);
-  }
-}
+//package com.graduation.blog.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+///**
+// * @Author: xiachuan
+// * @Date: 2019/2/18
+// * @Description:
+// */
+//@Configuration
+//public class CorsConfigure extends WebMvcConfigurerAdapter {
+//
+//  @Override
+//  public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")
+//        .allowedOrigins("*")
+//        .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+//        .allowCredentials(true).maxAge(3600);
+//  }
+//}
